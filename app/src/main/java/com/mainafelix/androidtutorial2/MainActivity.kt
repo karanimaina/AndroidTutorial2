548ae194ef4de44e56710cc9dc6ba8439d8e4430
felixmaina <karani.maina2010@gmail.com>
2022-05-10 01:45:03 +0300

    create a new instance of the Intent

--- app/src/main/java/com/mainafelix/androidtutorial2/MainActivity.kt
+++ app/src/main/java/com/mainafelix/androidtutorial2/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.mainafelix.androidtutorial2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val Button = findViewById<Button>(R.id.btnApply)
+        Button.setOnClickListener {
+          Intent(this,MainActivity2::class.java).also {
+              startActivity(it)
+          }
+        }
     }
 }
