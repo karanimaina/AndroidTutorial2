f99427eb7c6fd7633d0200899e9e3aaea7747905
felixmaina <karani.maina2010@gmail.com>
2022-05-10 23:50:12 +0300

    implements the on Item selected Listener ab=nd iherit from the Adapterview

--- app/src/main/java/com/mainafelix/androidtutorial2/SpinnerActivity.kt
+++ app/src/main/java/com/mainafelix/androidtutorial2/SpinnerActivity.kt
@@ -2,6 +2,8 @@ package com.mainafelix.androidtutorial2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.Spinner
 
 class SpinnerActivity : AppCompatActivity() {
@@ -9,6 +11,17 @@ class SpinnerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spinner)
         val spMonths = findViewById<Spinner>(R.id.spMonths)
+        //todo get the spMonths and set an onItem onItemSelectedListener then create an object that implements the AdapterView.onITem selected Listener
+       spMonths.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+           override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+               TODO("Not yet implemented")
+           }
 
+           override fun onNothingSelected(p0: AdapterView<*>?) {
+               TODO("Not yet implemented")
+           }
+
+
+       }
     }
 }
