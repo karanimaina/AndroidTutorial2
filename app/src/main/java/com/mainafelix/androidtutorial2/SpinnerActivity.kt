b4e0302fddc8291ed07f25bb7396c8655c3b6a09
felixmaina <karani.maina2010@gmail.com>
2022-05-10 23:41:47 +0300

    get the id of spmonths

--- app/src/main/java/com/mainafelix/androidtutorial2/SpinnerActivity.kt
+++ app/src/main/java/com/mainafelix/androidtutorial2/SpinnerActivity.kt
@@ -2,10 +2,13 @@ package com.mainafelix.androidtutorial2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Spinner
 
 class SpinnerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spinner)
+        val spMonths = findViewById<Spinner>(R.id.spMonths)
+
     }
 }
