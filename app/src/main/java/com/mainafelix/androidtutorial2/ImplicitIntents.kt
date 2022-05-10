e5680fbc61a4986af6426cd9874cb337d651ed92
felixmaina <karani.maina2010@gmail.com>
2022-05-10 16:12:54 +0300

    add a menu directory and create a menu bar"

--- app/src/main/java/com/mainafelix/androidtutorial2/ImplicitIntents.kt
+++ app/src/main/java/com/mainafelix/androidtutorial2/ImplicitIntents.kt
@@ -19,7 +19,7 @@ class ImplicitIntents : AppCompatActivity() {
             Intent(Intent.ACTION_GET_CONTENT).also {
               //get an image yhe / means to look for the type, available image options are jpeg, png,* lookks for all types etc
                 it.type= "image/*"
-                //start Activity and get theresult from the gallery app
+                //start Activity and get the result from the gallery app
                 startActivityForResult(it,0) // requesCode means we have away to differentiate several Activities that were started for result
 
             }
@@ -29,7 +29,7 @@ class ImplicitIntents : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         //check if the result was successful && requestcode == 0
-         if (resultCode == RESULT_OK&&requestCode==0){
+         if (resultCode == RESULT_OK && requestCode==0){
              // we can get the data inform of an intent , we can get
          // the actual path path from the  image that we just chose
              //the path to aresource is considered as uri
