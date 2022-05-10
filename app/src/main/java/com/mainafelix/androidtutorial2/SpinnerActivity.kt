1ea4ef17422315160bde24f197caaca003af8ecd
felixmaina <karani.maina2010@gmail.com>
2022-05-10 23:57:21 +0300

    modify the  on Item selected parameters

--- app/src/main/java/com/mainafelix/androidtutorial2/SpinnerActivity.kt
+++ app/src/main/java/com/mainafelix/androidtutorial2/SpinnerActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.Spinner
+import java.text.FieldPosition
 
 class SpinnerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,12 +14,12 @@ class SpinnerActivity : AppCompatActivity() {
         val spMonths = findViewById<Spinner>(R.id.spMonths)
         //todo get the spMonths and set an onItem onItemSelectedListener then create an object that implements the AdapterView.onITem selected Listener
        spMonths.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-           override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-               TODO("Not yet implemented")
+           override fun onItemSelected(AdapterView : AdapterView<*>?, view: View?,position:Int, id: Long) {
+
            }
 
-           override fun onNothingSelected(p0: AdapterView<*>?) {
-               TODO("Not yet implemented")
+           override fun onNothingSelected(AdapterView : AdapterView<*>?) {
+
            }
 
 
