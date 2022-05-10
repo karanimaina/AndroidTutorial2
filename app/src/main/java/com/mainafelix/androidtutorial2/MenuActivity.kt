a35a94d401733d7f8621228d1006f68d22d2ed33
felixmaina <karani.maina2010@gmail.com>
2022-05-10 16:52:21 +0300

    add when operator that selcts the specified menu item from the xml layout

--- app/src/main/java/com/mainafelix/androidtutorial2/MenuActivity.kt
+++ app/src/main/java/com/mainafelix/androidtutorial2/MenuActivity.kt
@@ -2,10 +2,32 @@ package com.mainafelix.androidtutorial2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+    }
+    // oveeride the oncreate menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //menu inflater is similar to layout inflater
+        menuInflater.inflate(R.menu.tool_barmenu,menu)
+     //this shows we have inflated our own menu
+        return true
+    }
+    // return a function that will dipllay some characteristics when the in the menyu are selected
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+      when(item.itemId){
+          R.id.miContacts ->Toast.makeText(this," contacts selected",Toast.LENGTH_LONG).show()
+          R.id.miSetting ->Toast.makeText(this," setting selected",Toast.LENGTH_LONG).show()
+          R.id.miFeedback ->Toast.makeText(this," feedback selected",Toast.LENGTH_LONG).show()
+          R.id.mifavorites ->Toast.makeText(this," favorites  selected",Toast.LENGTH_LONG).show()
+          R.id.miClose ->finish()
+      }
+
+        return true
     }
 }
