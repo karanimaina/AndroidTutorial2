bcb4e0c5a886c07c2f65d3786e153e52e4e946f2
felixmaina <karani.maina2010@gmail.com>
2022-05-11 13:18:56 +0300

    set an onClick Listener for the button to open various fragments

--- app/src/main/java/com/mainafelix/androidtutorial2/FragmentActivity.kt
+++ app/src/main/java/com/mainafelix/androidtutorial2/FragmentActivity.kt
@@ -2,10 +2,39 @@ package com.mainafelix.androidtutorial2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FragmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
+        //initialing the firstFRANEBT
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+      //todo  to inflate the content of our frame layout we need to use a frament transaction
+        supportFragmentManager.beginTransaction().apply {
+        // replace the fragment that with flFragment
+            replace(R.id.flFragment,firstFragment)
+            //commit applies the changes to our transaction
+            commit()
+        }
+        val btn1 = findViewById<Button>(R.id.btnFragment1)
+        val btn2 = findViewById<Button>(R.id.btnFragment2)
+        btn1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                // replace the fragment that with flFragment
+                replace(R.id.flFragment,firstFragment)
+                //commit applies the changes to our transaction
+                commit()
+            }
+        }
+        btn2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                // replace the fragment that with flFragment
+                replace(R.id.flFragment,secondFragment)
+                //commit applies the changes to our transaction
+                commit()
+            }
+        }
     }
 }
