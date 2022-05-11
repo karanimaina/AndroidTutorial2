ecef0a31e62b7c5dce33043e20f9c559d363d375
felixmaina <karani.maina2010@gmail.com>
2022-05-11 16:33:22 +0300

    create a view pager 2 class

--- app/src/main/java/com/mainafelix/androidtutorial2/BottomActivity.kt
+++ app/src/main/java/com/mainafelix/androidtutorial2/BottomActivity.kt
@@ -2,10 +2,39 @@ package com.mainafelix.androidtutorial2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class BottomActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom)
+      // creating fragment holders
+        val home = HomeFragment()
+        val message  = MessageFragment()
+        val profile = ProfileFragment()
+        setCurrentFragment(home)
+        val bottomView   = findViewById<BottomNavigationView>(R.id.BottomNavigationView)
+        bottomView.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                //set the menuIcons to  Specific fragments
+                R.id.miHome -> setCurrentFragment(home)
+                R.id.miMessage -> setCurrentFragment(message)
+                R.id.miProfile -> setCurrentFragment(profile)
+            }
+            true
+        }
+     // use the get or create badge to show the message Count
+        bottomView.getOrCreateBadge(R.id.miMessage).apply {
+            number =10
+            isVisible= true
+        }
+
+    }
+    //create a function that replaces our current frag
+    // ment with a new one
+    private fun  setCurrentFragment(fragment: Fragment) = supportFragmentManager.beginTransaction().apply {
+        replace (R.id.flBottomFragment,fragment)
+        commit()
     }
 }
