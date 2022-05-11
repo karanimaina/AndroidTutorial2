012729ae5aba86b8e60b6ee8d2e6e52c2de893bb
felixmaina <karani.maina2010@gmail.com>
2022-05-11 23:46:21 +0300

    sybnc toggle sstate

--- app/src/main/java/com/mainafelix/androidtutorial2/SlidableMenuActivity.kt
+++ app/src/main/java/com/mainafelix/androidtutorial2/SlidableMenuActivity.kt
@@ -2,8 +2,10 @@ package com.mainafelix.androidtutorial2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.drawerlayout.widget.DrawerLayout
 
 class SlidableMenuActivity : AppCompatActivity() {
     //late init means we will initialize the variabble later on we don't have to give it a value
@@ -11,8 +13,21 @@ class SlidableMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_slidable_menu)
-        val drawerLayout = findViewById<ConstraintLayout>(R.id.drawerLayout)
-        toggle = ActionBarDrawerToggle(this,drawerLayout,)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        //assign toggle to the Action Bar drawe object
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open, R.string.close)
+        //add a drawer listener
+        drawerLayout.addDrawerListener(toggle)
+        // tell toggle to sync its state, to make it ready to use
+        toggle.syncState()
+// support action bar produces a bac
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return  true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
