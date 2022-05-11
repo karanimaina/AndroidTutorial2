25ecb3848393496d8cea4aa3bc5ebb9a90c7f907
felixmaina <karani.maina2010@gmail.com>
2022-05-11 23:30:03 +0300

    modify the nav_header and nav  drawe_menu

--- app/src/main/java/com/mainafelix/androidtutorial2/SlidableMenuActivity.kt
+++ app/src/main/java/com/mainafelix/androidtutorial2/SlidableMenuActivity.kt
@@ -2,10 +2,17 @@ package com.mainafelix.androidtutorial2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class SlidableMenuActivity : AppCompatActivity() {
+    //late init means we will initialize the variabble later on we don't have to give it a value
+    lateinit var toggle :ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_slidable_menu)
+        val drawerLayout = findViewById<ConstraintLayout>(R.id.drawerLayout)
+        toggle = ActionBarDrawerToggle(this,drawerLayout,)
+
     }
 }
