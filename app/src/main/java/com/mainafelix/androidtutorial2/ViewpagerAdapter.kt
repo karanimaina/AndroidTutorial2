b0e6d98d8dcbe5d522a87818275feee05be61d18
felixmaina <karani.maina2010@gmail.com>
2022-05-11 17:01:45 +0300

    create a view pager arecycler Adapter

--- app/src/main/java/com/mainafelix/androidtutorial2/ViewpagerAdapter.kt
+++ app/src/main/java/com/mainafelix/androidtutorial2/ViewpagerAdapter.kt
@@ -1,7 +1,33 @@
 package com.mainafelix.androidtutorial2
 
-class ViewpagerAdapter(
-    mut
-) {
+import android.media.Image
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class ViewpagerAdapter (
+  var images :List<Int>// the added images  are in the drawable folder and they are reffered byId
+):RecyclerView.Adapter<ViewpagerAdapter.ViewPagerViewHolder>() {
+    inner class ViewPagerViewHolder (itemView: View):RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerViewHolder {
+      val view = LayoutInflater.from(parent.context).inflate(R.layout.item_viewpager2,parent,false)
+        return ViewPagerViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewPagerViewHolder, position: Int) {
+        val currImage =images[position]
+      holder.itemView.apply {
+          val image = findViewById<ImageView>(R.id.IvImage)
+          image.setImageResource(currImage)
+      }
+    }
+
+    override fun getItemCount(): Int {
+        return  images.size
+    }
+
 
 }
