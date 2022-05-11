4eb75d9edb256cc23fea9f7fa4b0ba9fe4da6d61
felixmaina <karani.maina2010@gmail.com>
2022-05-11 20:25:26 +0300

    add selected add an ontab selected Listener to the tab

--- app/src/main/java/com/mainafelix/androidtutorial2/ViewPager2Activity.kt
+++ app/src/main/java/com/mainafelix/androidtutorial2/ViewPager2Activity.kt
@@ -2,8 +2,11 @@ package com.mainafelix.androidtutorial2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class ViewPager2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,11 +24,34 @@ class ViewPager2Activity : AppCompatActivity() {
          val view = findViewById<ViewPager2>(R.id.viewPager)
         view.adapter = adapter
        // set vertical swipe
-        view.orientation= ViewPager2.ORIENTATION_VERTICAL
-        //set programmatic dragging
-        view.beginFakeDrag()
-        //-10 is an offset ofhow string we want to drag
-        view.fakeDragBy(-10f)// - means we want to drag to the lft
-        view.endFakeDrag()
+//        view.orientation= ViewPager2.ORIENTATION_VERTICAL
+//        //set programmatic dragging
+//        view.beginFakeDrag()
+//        //-10 is an offset ofhow string we want to drag
+//        view.fakeDragBy(-10f)// - means we want to drag to the lft
+//        view.endFakeDrag()
+val  tablayout = findViewById<TabLayout>(R.id.tbTabLayout)
+     //tab layout connects our viewpager and
+        TabLayoutMediator(tablayout,view){tab , position ->
+            tab.text= "Tab ${position+1}"
+        }.attach()
+        //responding to touch click events
+                                     // an anonymous class
+        tablayout.addOnTabSelectedListener(object :TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+           Toast.makeText(applicationContext,"tab selected",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                Toast.makeText(applicationContext,"tab Unselected",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                Toast.makeText(applicationContext,"tab reselected",Toast.LENGTH_SHORT).show()
+
+            }
+
+        })
+
     }
 }
