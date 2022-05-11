fd897a5ad2c9084ec1ab906ced4fbc9a75b2ded5
felixmaina <karani.maina2010@gmail.com>
2022-05-11 23:59:21 +0300

    edit the nav view

--- app/src/main/java/com/mainafelix/androidtutorial2/SlidableMenuActivity.kt
+++ app/src/main/java/com/mainafelix/androidtutorial2/SlidableMenuActivity.kt
@@ -6,6 +6,9 @@ import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.internal.NavigationMenu
+import com.google.android.material.navigation.NavigationBarMenu
+import com.google.android.material.navigation.NavigationView
 
 class SlidableMenuActivity : AppCompatActivity() {
     //late init means we will initialize the variabble later on we don't have to give it a value
@@ -22,6 +25,12 @@ class SlidableMenuActivity : AppCompatActivity() {
         toggle.syncState()
 // support action bar produces a back arrow that closses the nav bar
     supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val nav = findViewById<NavigationView>(R.id.navView)
+        nav.setNavigationItemSelectedListener {
+            when(it.itemId){
+
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
