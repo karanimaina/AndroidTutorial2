478ee57756ca0aadc98ea1251ff8b70814a69a35
felixmaina <karani.maina2010@gmail.com>
2022-05-11 20:52:41 +0300

    add aslidable menu Activity

---
+++ app/src/main/java/com/mainafelix/androidtutorial2/SlidableMenuActivity.kt
@@ -0,0 +1,11 @@
+package com.mainafelix.androidtutorial2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SlidableMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_slidable_menu)
+    }
+}
