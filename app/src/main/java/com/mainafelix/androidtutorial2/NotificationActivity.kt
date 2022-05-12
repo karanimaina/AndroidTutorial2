336e66355262cff4c05fd5636b4aea16e12d482e
felixmaina <karani.maina2010@gmail.com>
2022-05-12 17:04:22 +0300

    set the notification manager to read notification from this actiity

--- app/src/main/java/com/mainafelix/androidtutorial2/NotificationActivity.kt
+++ app/src/main/java/com/mainafelix/androidtutorial2/NotificationActivity.kt
@@ -42,6 +42,7 @@ class NotificationActivity : AppCompatActivity() {
             .setPriority(NotificationCompat.PRIORITY_HIGH)
                 // return anotification here
             .setContentIntent(pendingIntent)
+
             .build()
         val notificationManager = NotificationManagerCompat.from(this)
         val button = findViewById<Button>(R.id.noti)
