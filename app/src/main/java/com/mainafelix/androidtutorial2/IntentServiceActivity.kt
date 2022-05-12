1f9e8fb6fda1b780c69cedd1289de5e6290f30c2
felixmaina <karani.maina2010@gmail.com>
2022-05-12 18:35:39 +0300

    add intent Serviceto to the manifest.xml

--- app/src/main/java/com/mainafelix/androidtutorial2/IntentServiceActivity.kt
+++ app/src/main/java/com/mainafelix/androidtutorial2/IntentServiceActivity.kt
@@ -1,13 +1,29 @@
 package com.mainafelix.androidtutorial2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class IntentServiceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent_service)
-         val buttonStart = findViewById<Button>(R.id.bt)
+         val buttonStart = findViewById<Button>(R.id.btnIntent)
+         val buttonStop = findViewById<Button>(R.id.btnService)
+        val tvService  = findViewById<TextView>(R.id.tvService)
+        buttonStart.setOnClickListener {
+            Intent(this,IntentServiceClass::class.java).also{
+                startService(it)
+                tvService.text = "service is running"
+            }
+        }
+        buttonStop.setOnClickListener {
+          IntentServiceClass.stopService()
+            tvService.text =" service stopped"
+            //add Intent service to the manifest class
+
+        }
     }
 }
