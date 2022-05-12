7ef559d0314a0eba46c200b190f816a837301d72
felixmaina <karani.maina2010@gmail.com>
2022-05-12 18:24:46 +0300

    spcify the Id of the buttons

--- app/src/main/java/com/mainafelix/androidtutorial2/IntentServiceActivity.kt
+++ app/src/main/java/com/mainafelix/androidtutorial2/IntentServiceActivity.kt
@@ -2,10 +2,12 @@ package com.mainafelix.androidtutorial2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class IntentServiceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent_service)
+         val buttonStart = findViewById<Button>(R.id.bt)
     }
 }
